ef7b8b827e6464e58d763266708688c93a7cab49
Ankit Sharma <ankitsm08@gmail.com>
2024-09-24 00:07:26 +0530

    Deploy compiled Jekyll site

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,33 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1727115930',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/all-posts/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/about/',
+      
+        '/sponsor/',
+      
+
+      
+      
+        '/assets/js/dist/app.min.js',
+        '/assets/js/dist/categories.min.js',
+        '/assets/js/dist/commons.min.js',
+        '/assets/js/dist/home.min.js',
+        '/assets/js/dist/misc.min.js',
+        '/assets/js/dist/page.min.js',
+        '/assets/js/dist/post.min.js'
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
