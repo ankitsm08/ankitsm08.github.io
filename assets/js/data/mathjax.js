09e7d933ef55edb3cb6547b41c638ebe233b4a9b
Ankit Sharma <ankitsm08@gmail.com>
2024-09-25 14:37:37 +0530

    line brak preservance config

--- assets/js/data/mathjax.js
+++ assets/js/data/mathjax.js
@@ -1,1 +1,10 @@
-MathJax = { tex: {inlineMath: [ ['$', '$'], ['\\(', '\\)'] ],displayMath: [ ['$$', '$$'], ['\\[', '\\]'] ],tags: 'ams' } };
+MathJax = {
+  tex: {inlineMath: [
+      ['$', '$'],
+      ['\\(', '\\)']
+    ],displayMath: [
+      ['$$', '$$'],
+      ['\\[', '\\]']
+    ],tags: 'ams'
+  }
+}; <table id="compress_html_profile_20240925" class="compress_html_profile"> <thead> <tr> <td>Step <td>Bytes <tbody> <tr> <td>raw <td>160 <tr> <td>endings <td>161 <tr> <td>comments <td>161 <tr> <td>collapse <td>160 <tr> <td>clippings <td>160 </table>
--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,13 @@
-const swconf = { cacheName: 'chirpy-1727254700',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/all-posts/', '/categories/', '/archives/', '/tags/', '/about/', ],denyPaths: [ ], purge: false };
+const swconf = {
+    cacheName: 'chirpy-1727255224',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+        '/all-posts/',
+        '/categories/',
+        '/archives/',
+        '/tags/',
+        '/about/',
+    ],denyPaths: [
+    ],
+    purge: false
+}; <table id="compress_html_profile_20240925" class="compress_html_profile"> <thead> <tr> <td>Step <td>Bytes <tbody> <tr> <td>raw <td>343 <tr> <td>endings <td>344 <tr> <td>comments <td>344 <tr> <td>collapse <td>273 <tr> <td>clippings <td>273 </table>
