68f75ac0d85219c4648b2caebed347967f08b21c
Ankit Sharma <ankitsm08@gmail.com>
2024-09-26 21:12:55 +0530

    update 0.1.1

--- assets/js/data/mathjax.js
+++ assets/js/data/mathjax.js
@@ -7,4 +7,4 @@ MathJax = {
       ['\\[', '\\]']
     ],tags: 'ams'
   }
-}; <table id="compress_html_profile_20240926" class="compress_html_profile"> <thead> <tr> <td>Step <td>Bytes <tbody> <tr> <td>raw <td>160 <tr> <td>endings <td>161 <tr> <td>comments <td>161 <tr> <td>collapse <td>160 <tr> <td>clippings <td>160 </table>
+};
--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,3 +1,3 @@
 const swconf = {
     purge: true
-}; <table id="compress_html_profile_20240926" class="compress_html_profile"> <thead> <tr> <td>Step <td>Bytes <tbody> <tr> <td>raw <td>42 <tr> <td>endings <td>43 <tr> <td>comments <td>43 <tr> <td>collapse <td>36 <tr> <td>clippings <td>36 </table>
+};
