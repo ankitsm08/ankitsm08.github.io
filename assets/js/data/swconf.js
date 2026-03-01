5ee1572a4c2041263560da93221b59164af7eed3
Ankit Sharma <ankitsm08@gmail.com>
2026-03-01 21:54:49 +0530

    update 0.4.1-quickfix
    - commit acutal built site
    - include service worker destroyer

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,34 @@
 const swconf = {
   
-    purge: true
+    cacheName: 'chirpy-1772382016',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/all-posts/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/my-docs/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+          'https://ankitsm08.goatcounter.com/counter/'
+        
+      ]
+    },
+
+    purge: false
   
 };
 
